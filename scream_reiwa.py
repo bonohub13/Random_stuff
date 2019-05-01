76239b952ba1b4fbc50854c5b30652fbfedb00df
Kensuke Saito <33274231+bonohub13@users.noreply.github.com>
2019-05-01 15:20:55 +0900

    Add files via upload

---
+++ scream_reiwa.py
@@ -0,0 +1,17 @@
+class Reiwa:
+    def scream_reiwa(self):
+        for i in range(10):
+            print('令和だよ！ＲＥＩＷＡ＊＊＊ＲＥＩＷＡ＊＊＊ＲＥＩＷＡ!!!')
+        print('平成でやったこと超えられますように！！！！！！！！！！')
+        print('m~(・ω・`m~) ｺｴﾗﾚﾅｶｯﾀﾗ､ﾜｶｯﾃﾙﾅ?')
+
+    def loop(self):
+        end = False
+        while not end:
+            button = input('=== ENTER to escape ===')
+            if button is '':
+                end = True
+
+if __name__ == "__main__":
+    Reiwa().scream_reiwa()
+    Reiwa().loop()
